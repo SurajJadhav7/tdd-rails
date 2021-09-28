e72304bc4dfcfa84540a1ff852af2fe3e9b791ce
Suraj Jadhav <jadhavsuraj777@gmail.com>
2021-09-28 22:16:09 +0530

    added email owned todos

--- Gemfile
+++ Gemfile
@@ -50,4 +50,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :test do
   gem 'capybara', '~> 2.7.1'
+  gem 'database_cleaner'
 end
--- spec/support/features/sign_in.rb
+++ spec/support/features/sign_in.rb
@@ -1,7 +1,10 @@
 module Features
     def sign_in
+        sign_in_as "johndoe@example.com"
+    end
+    def sign_in_as(email)
         visit root_path
-        fill_in 'Email', with: "johndoe@example.com"
+        fill_in 'Email', with: email
         click_on 'Sign in'
     end
 end
